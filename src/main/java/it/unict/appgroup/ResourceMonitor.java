d04b859067f3292cb0b3b93f325de584390694cd
Angelo Marchese <marchese.angelo1@gmail.com>
2024-08-25 15:28:35 +0200

    Fix issue with deployment annotations assignment

--- src/main/java/it/unict/appgroup/ResourceMonitor.java
+++ src/main/java/it/unict/appgroup/ResourceMonitor.java
@@ -8,6 +8,8 @@ public class ResourceMonitor {
         appGroupGraph.getApps().forEach(app -> {
             app.getDeployment().getMetadata().getAnnotations().put("cpu-usage", String.valueOf(app.getCpuUsage()));
             app.getDeployment().getMetadata().getAnnotations().put("memory-usage", String.valueOf(app.getMemoryUsage()));
+            app.getDeployment().getMetadata().getAnnotations().put("network-usage", String.valueOf(app.getNetworkBandwidthUsage()));
+            app.getDeployment().getMetadata().getAnnotations().put("disk-usage", String.valueOf(app.getDiskBandwidthUsage()));
         });
     }
 }
